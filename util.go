42bdbeeec9365628022c7a8a808448f4bf44d42b
Jelle van den Hooff <jelle@vandenhooff.name>
2015-03-26 23:37:24 -0400

    Small API for external packages to extract information from VerifiedEmail.

---
+++ util.go
@@ -0,0 +1,15 @@
+package dkim
+
+import "strings"
+
+func (v *VerifiedEmail) CanonHeaders() string {
+	var canonHeaders []string
+	for _, header := range append(v.Headers, v.Signature.canonHeader) {
+		canonHeaders = append(canonHeaders, v.Signature.canon.header(header))
+	}
+	return strings.Join(canonHeaders, "")
+}
+
+func (v *VerifiedEmail) ExtractHeaders(name string) []string {
+	return extractHeaders(v.Headers, []string{name})
+}
